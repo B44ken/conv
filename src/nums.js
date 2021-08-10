91decd83ee4cc023d099abdb2d936067400cb573
brad boratto <bradboratto@gmail.com>
2021-08-10 07:53:19 -0400

    create unit() instead of declaring objects

--- src/nums.js
+++ src/nums.js
@@ -1,4 +1,4 @@
-	export const prefixes = { 
+export const prefixes = {
 	yotta: 1e+24,
 	zetta: 1e+21,
 	exa: 1000000000000000000,
@@ -20,34 +20,48 @@
 	zepto: 1e-21,
 	yocto: 1e-24,
 }
+
+
+const unit = (derived) => {
+	var unitObject = { ..._ }
+	for (const part of derived.split(" ")) {
+		var [name, amount] = part.split('^')
+		if (amount) amount = parseInt(amount)
+		else amount = 1
+		unitObject[name] = amount
+	}
+	return unitObject
+}
+
 // the base unit so we don't have to keep recording zeroes 
-const  _ = { m: 0, s: 0, mole: 0, A: 0, K: 0, cd: 0, kg: 0 }
+const _ = { m: 0, s: 0, mole: 0, A: 0, K: 0, cd: 0, kg: 0 }
 export const units = {
 	_,
-	gram: { ..._, g: 1 },
-	meter: { ..._, m: 1},
-	second: { ..._, s: 1 },
-	amp: { ..._, A: 1 },
-	hertz: { ..._, s: -1 },
-	watt: { ..._, kg: 1, m: 2, s:  -3 },
-	volt: { ..._, m: 2, s: -3, kg: 1, A: -1 },
-	newton: { ..._, kg: 1, m: 1, s: -2 },
-	pascal: { ..._, kg: 1, m: -1, s: -2 },
-	joule: { ..._, kg: 1, m: 2, s: -2 }
+	gram: unit('g'),
+	meter: unit('m'),
+	second: unit('s'),
+	amp: unit('A'),
+	hertz: unit('s^-1'),
+	watt: unit('kg m^2 s^-3'),
+	volt: unit('m^2 s^-3 kg A^1'),
+	newton: unit('kg m s^-2'),
+	pascal: unit('kg m^-1 s^-2'),
+	joule: unit('kg m^2 s^-2'),
+	coulomb: unit('A s^-1')
 }
 
 
 // take a derivation like { m: 1 } and turn it into a unit name like 'meter'
 export const reverseFactor = (derived) => {
 	// loop through existing names (meter, volt, etc.)
-	for(var u in units) {
-		if(JSON.stringify(units[u]) == JSON.stringify(derived) && u != '_') {
+	for (var u in units) {
+		if (JSON.stringify(units[u]) == JSON.stringify(derived) && u != '_') {
 			return u
 		}
-	}	
+	}
 	// if not, make a derived name like m/s = (m^1 s^1)
 	var derivedName = ''
-	for(u in derived) {
+	for (u in derived) {
 		if (derived[u] == 0) continue
 		derivedName += u + '^' + derived[u] + ' '
 	}
