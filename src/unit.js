66bc81eee4064c505e86750e4b180593a2279632
brad boratto <bradboratto@gmail.com>
2021-08-10 07:54:19 -0400

    throw an error if unit cannot be found or derived

--- src/unit.js
+++ src/unit.js
@@ -1,14 +1,15 @@
+import { CalcError } from './calc.js'
 import { prefixes, units, reverseFactor } from './nums.js'
 
 export class Factor {
-    constructor(number=1, derived={ ...units._ }) {
-        this.number = number
+    constructor(number = 1, derived = { ...units._ }) {
+        this.number = number || 1
         if (typeof derived == "string") {
             this.derived = { ...units._, ...units[derived] }
             this.name = derived
         }
         else {
-	        var derived = { ...units._, ...derived }
+            var derived = { ...units._, ...derived }
             this.derived = derived
             this.name = this.unitName(this.derived)
         }
@@ -16,32 +17,32 @@ export class Factor {
 
     static fromString(str) {
         var unit = new Factor()
-		
-		// build a regex to find any string like "100 milliamp"
+
+        // build a regex to find any string like "100 milliamp"
         var p = Object.keys(prefixes)
         var u = Object.keys(units)
         const regex = RegExp(`([0-9\.]+)? ?(?:(${p.join('|')})?((${u.join('|')})s?))`)
-	    var exec = regex.exec(str)
+        var exec = regex.exec(str)
 
-        if(exec == null) {
+        if (exec == null) {
             var num = Number(str)
-            if(isNaN(num)) 
-                return null
-            if(str == '')
-                return new Factor() 
+            if (isNaN(num))
+                throw new CalcError('could not derive: ' + str)
+            if (str == '')
+                return new Factor()
             return new Factor(num)
 
         }
 
-        for(const part of exec) {
+        for (const part of exec) {
             if (part == exec[1]) {
                 unit.number = Number(part)
             }
-            else if(p.includes(part)) {
+            else if (p.includes(part)) {
                 var x = new Factor(prefixes[part])
                 unit = unit.multiply(x)
             }
-            else if(u.includes(part)) {
+            else if (u.includes(part)) {
                 unit = new Factor(unit.number, units[part])
             }
         }
@@ -61,15 +62,15 @@ export class Factor {
         var num = this.number * newFactor.number
         var derived = { ...units._ }
         var product = new Factor(num, derived)
-        for(var u in { ...units._ }) {
+        for (var u in { ...units._ }) {
             product.derived[u] = this.derived[u] + newFactor.derived[u]
         }
         return product
     }
 
-    divide(newFactor) {   
+    divide(newFactor) {
         var quot = new Factor(this.number / newFactor.number, this.derived)
-        for(var base of Object.keys(newFactor.derived))
+        for (var base of Object.keys(newFactor.derived))
             quot.derived[base] -= newFactor.derived[base]
         return quot
     }
@@ -77,7 +78,7 @@ export class Factor {
     add(newFactor) {
         var derived1 = JSON.stringify(this.derived)
         var derived2 = JSON.stringify(newFactor.derived)
-        if(derived1 == derived2) {
+        if (derived1 == derived2) {
             newFactor.number += this.number
             return newFactor
         }
@@ -86,7 +87,7 @@ export class Factor {
     subtract(newFactor) {
         var derived1 = JSON.stringify(this.derived)
         var derived2 = JSON.stringify(newFactor.derived)
-        if(derived1 == derived2) {
+        if (derived1 == derived2) {
             newFactor.number = this.number - newFactor.number
             return newFactor
         }
