df1ddbe673c6be219195d5c3a1d17610f36469b9
b44ken <bradboratto@gmail.com>
2021-11-24 20:29:17 -0500

    add non si units

--- nums.js
+++ nums.js
@@ -22,7 +22,7 @@ export const prefixes = {
 }
 
 const unit = (derived, scale = 1) => {
-	var unitObject = { ..._ }
+	var unitObject = { ..._, SCALE: scale }
 	for (const part of derived.split(" ")) {
 		var [name, amount] = part.split('^')
 		if (amount) amount = parseInt(amount)
@@ -32,26 +32,43 @@ const unit = (derived, scale = 1) => {
 	return unitObject
 }
 
-// the base unit so we don't have to keep recording zeroes 
-const _ = { m: 0, s: 0, mol: 0, A: 0, K: 0, cd: 0, kg: 0 }
+// the base unit so we don't have to keep recording zeroes
+// all SI base units plus B, for bit
+// unitName: [ factor w/ si equivalent, si derivation ]
+const _ = { m: 0, s: 0, mol: 0, A: 0, K: 0, cd: 0, kg: 0, b: 0 }
 export const units = {
 	_,
-	gram: unit('kg', 0.001),
-	meter: unit('m'),
-	second: unit('s'),
-	amp: unit('A'),
-	kelvin: unit('K'),
-	mole: unit('mol'),
-	hertz: unit('s^-1'),
-	watt: unit('kg m^2 s^-3'),
-	volt: unit('m^2 s^-3 kg A^-1'),
-	newton: unit('kg m s^-2'),
-	pascal: unit('kg m^-1 s^-2'),
-	joule: unit('kg m^2 s^-2'),
-	coulomb: unit('A s^-1'),
-	mile: unit('m', 1609.34),
-	minute: unit('s', 60),
-	hour: unit('s', 3600)
+	"meter": [1, { ..._, "m": 1 }],
+	"second": [1, { ..._, "s": 1 }],
+	"amp": [1, { ..._, "A": 1 }],
+	"kelvin": [1, { ..._, "K": 1 }],
+	"mole":	[1, { ..._, "mol": 1 }],
+	"hertz": [1, { ..._, "s": -1 }],
+	"watt": [1, { ..._, "kg": 1, "m": 2, "s": -3 }],
+	"volt": [1, { ..._, "kg": 1, "m": 2, "s": -3, "A": -1 }],
+	"newton": [1, { ..._, "kg": 1, "m": 1, "s": -2 }],
+	"pascal": [1, { ..._, "kg": 1, "m": -1, "s": -2 }],
+	"joule": [1, { ..._, "kg": 1, "m": 2, "s": -2 }],
+	"coulomb": [1, { ..._, "s": -1, "A": 1 }],
+	"bit": [1, { ..._, "b": 1 }],
+	"byte": [8, { ..._, "b": 1 }],
+
+	// SI must be listed first so reverseFactor finds them first - bad code?
+	"mile": [1609.344, { ..._, "m": 1 }],
+	"yard": [0.9144, { ..._, "m": 1 }],
+	"foot": [0.3048, { ..._, "m": 1 }],
+	"inch": [0.0254, { ..._, "m": 1 }],
+	"pound": [0.45359237, { ..._, "kg": 1 }],
+	"ounce": [0.028349523125, { ..._, "kg": 1 }],
+	"gallon": [3.785411784, { ..._, "m": 3 }],
+	"quart": [0.946352946, { ..._, "m": 3 }],
+	"pint": [0.473176473, { ..._, "m": 3 }],
+	"cup": [0.2365882365, { ..._, "m": 3 }],
+	"gram": [0.001, { ..._, "kg": 1 }],
+	"minute": [60, { ..._, "s": 1 }],
+	"hour": [3600, { ..._, "s": 1 }],
+	"poundal": [0.138254954376, { ..._, "kg": 1, "m": 1, "s": -2 }],	
+	"psi": [6894.75729317, { ..._, "kg": 1, "m": -1, "s": -2 }],
 }
 
 
@@ -59,10 +76,11 @@ export const units = {
 export const reverseFactor = (derived) => {
 	// loop through existing names (meter, volt, etc.)
 	for (var u in units) {
-		if (JSON.stringify(units[u]) == JSON.stringify(derived) && u != '_') {
+		if (JSON.stringify(units[u][1]) == JSON.stringify(derived) && u != '_') {
 			return u
 		}
 	}
+
 	// if not, make a derived name like m/s = (m^1 s^1)
 	var derivedName = ''
 	for (u in derived) {
