91aadf48836cb14780f7c0cab87ac674617478c5
b44ken <bradboratto@gmail.com>
2022-03-23 20:53:00 -0400

    header tabs

--- page.js
+++ page.js
@@ -2,21 +2,42 @@ import { Factor } from './unit.js'
 import { Calculator, CalcError } from './calc.js'
 import { testSuite } from './test.js'
 
+// makes my life a bit easier when debugging
 window.Factor = Factor
 window.Calculator = Calculator
 window.CalcError = CalcError
 window.testSuite = testSuite
-
-
 window.calc = new Calculator({ 'precision': 1e-5 })
 
 const input = document.querySelector('.input')
 const output = document.querySelector('.output')
 
-output.textContent = calc.doLine(input.textContent)
+var currentSelected = null
+const manageTab = name => {
+    const button = document.getElementsByClassName(name + '-button')[0]
+    const content = document.getElementsByClassName(name + '-tab')[0]
+    button.addEventListener("click", event => {
+        if(currentSelected) currentSelected.style.display = "none"
+        currentSelected = content
+        content.style.display = "block"
+    })
+}
 
-document.addEventListener('keyup', () => {
-    calc = new Calculator({ 'precision': 1e-5 })
+manageTab('settings')
+manageTab('formulas')
+manageTab('close')
+
+var settings = {
+    precision: 1e-5,
+    properSigFigs: false
+}
+
+document.getElementById("settings-proper").addEventListener("change", event => {
+    settings.properSigFigs = event.target.checked
+})
+
+const doInput = () => {
+    calc = new Calculator(settings)
     try {
         var res = ""
         var cleanInput = input.innerText.split('\n').filter(e=> e != '')
@@ -29,4 +50,10 @@ document.addEventListener('keyup', () => {
             console.error(error)
         output.textContent = 'error: ' + error.message
     }
-})
+}
+
+document.addEventListener('keyup', doInput)
+
+var search = decodeURI(document.location.search).replace('?', '').replaceAll(':','\n')
+if(search) input.innerText = search
+doInput()
