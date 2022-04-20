f468158025538725707bda790c8858bd71706a2a
b44ken <bradboratto@gmail.com>
2022-04-20 10:20:28 -0400

    more tests (some fail)

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 // test suite is automatically run
-// s.doTest(test case name, value to put into calculator, expected result)
+// s.test(test case name, value to put into calculator, expected result)
 
 import { Calculator } from './calc.js' 
 
@@ -8,16 +8,18 @@ class TestSuite {
         this.calc = new Calculator()
         this.results = []
     }
-    doTest(name, input, expected) {
+    test(name, input, expected) {
         try {
             var result = this.calc.doLine(input).trim()
+            if(result == expected) {
+                this.results.push({ name, input, expected, result, error: null })
+            } else {
+                this.results.push({ name, input, expected, result, error: "Wrong" })
+            }
         } catch(error) {
             this.results.push({ name, input, expected, result: null, error })
         } finally {
-            if(result == expected)
-                this.results.push({ name, input, expected, result, error: null })
-            else
-                this.results.push({ name, input, expected, result, error: "Wrong" })
+
         }
     }
     summary() {
@@ -29,16 +31,24 @@ class TestSuite {
 
 var s = new TestSuite()
 
-s.doTest('metric declaration',      '1 amp',              '1 amp')
-s.doTest('addition',                '1 + 1',              '2')
-s.doTest('multiplication',          '1 * 1',              '1')
-s.doTest('power',                   '2 ^ 3',              '8')
-s.doTest('imperial declaration',    '1 mile',             '1609.344 meter')
-s.doTest('variable assignment',     'var = 3',            '3')
-s.doTest('variable usage',          'var + 2',            '5')
-s.doTest('power of unit',           '1 meter^2',          '1 m^2')
-s.doTest('compounding units',       '1 newton / meter^2', '1 pascal')
-s.doTest('simple bedmas',           '2 + 4*2',            '10')
+// s.test('error on purpose',    'garbage',          '')
+// s.test('wrong on purpose',    '1',                '2')
+
+s.test('metric declaration',  '1 amp',            '1 amp')
+s.test('addition',            '1 + 1',            '2')
+s.test('multiplication',      '1 * 1',            '1')
+s.test('power',               '2 ^ 3',            '8')
+s.test('imperial',            '1 mile',           '1609.344 meter')
+s.test('variable',            'var = 3',          '3')
+s.test('variable usage',      'var + 2',          '5')
+s.test('power of unit',       '1 meter^2',        '1 m^2')
+s.test('compounding units',   '1 newton / M^2',   '1 pascal')
+s.test('simple bedmas',       '2 + 4*2',          '10')
+s.test('KG is kilogram',      '5 KG',             '5 kilogram')
+s.test('K is kelvin',         '5 K',              '5 kelvin')
+s.test('MPa is megapascal',   '2 MPa',            '2000000 pascal')
+s.test('brackets exist',      '(8)',              '8')
+s.test('brackets work',       '4 * (2 + 2)',      '16')
 
 s.summary()
 
