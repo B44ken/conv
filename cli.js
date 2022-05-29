9082b3ea0b553059d9a84055908a381b49e1871d
b44ken <bradboratto@gmail.com>
2022-05-28 20:01:18 -0400

    run from node

---
+++ cli.js
@@ -0,0 +1,18 @@
+import { Factor } from './unit.js'
+import { Calculator, CalcError } from './calc.js'
+
+const calc = new Calculator({ 'precision': 1e-5 })
+
+var settings = {
+    precision: 1e-5,
+    properSigFigs: false
+}
+
+const arg = process.argv[2]
+try {
+	console.log(calc.doLine(arg))
+} catch(error) {
+	if(error instanceof CalcError)
+		console.error("error: " + error.message)
+	else throw error
+}
